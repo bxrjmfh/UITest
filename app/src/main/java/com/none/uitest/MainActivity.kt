6f4b81bc73b3e1dae5002f289d521a72e9f1b276
bxrjmfh <79852164+bxrjmfh@users.noreply.github.com>
2021-09-11 14:24:54 +0800

    使用了button

--- app/src/main/java/com/none/uitest/MainActivity.kt
+++ app/src/main/java/com/none/uitest/MainActivity.kt
@@ -2,12 +2,28 @@ package com.none.uitest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.none.uitest.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(), View.OnClickListener
 {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding_1=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding_1.root)
+        /*binding_1.button1.setOnClickListener(){
+           //一种按钮方法的写法
+        }*/
+    }
+
+    override fun onClick(p0: View?)
+    {
+        when(p0?.id){
+            R.id.button_1->{
+                //添加逻辑的第二种方法
+            }
+        }
     }
 }
